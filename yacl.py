c7e222ec8dd494712c12cb97f7bd7d5ee334c663
Ingo Meyer <i.meyer@fz-juelich.de>
2022-05-18 09:46:52 +0200

    Increase the version number to v0.4.4

--- yacl.py
+++ yacl.py
@@ -21,7 +21,7 @@ __author__ = "Ingo Meyer"
 __email__ = "i.meyer@fz-juelich.de"
 __copyright__ = "Copyright © 2021 Forschungszentrum Jülich GmbH. All rights reserved."
 __license__ = "MIT"
-__version_info__ = (0, 4, 3)
+__version_info__ = (0, 4, 4)
 __version__ = ".".join(map(str, __version_info__))
 
 
